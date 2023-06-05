ada6ef2c8ab6a8432aa39b92a96dbeb40abae2cd
dineshdnryb <datacampdnr@gmail.com>
2023-06-05 23:07:32 +0530

    first

---
+++ set_matrix_zeros.py
@@ -0,0 +1,16 @@
+def setZeros(matrix: List[List[int]]) -> None:
+	# Write your code here.
+    n=len(matrix)
+    m=len(matrix[0])
+    row=[0]*n
+    col=[0]*m
+    for i in range(n):
+        for j in range(m):
+            if(matrix[i][j]==0):
+                row[i]=1
+                col[j]=1
+    for i in range(n):
+        for j in range(m):
+            if(row[i] or col[j]):
+                matrix[i][j]=0
+    return matrix
