1f8af65fe3bf0c4abf9431a2591876f48771af66
dineshdnryb <datacampdnr@gmail.com>
2023-06-06 22:39:16 +0530

    Day 2

---
+++ Pascals_Triangle.py
@@ -0,0 +1,14 @@
+def printPascal(n:int):
+    # Write your code here.
+    # Return a list of lists.
+    ans=[[1]]
+    for row in range(1,n):
+        tempcol=[]
+        for col in range(row+1):
+            if(col-1<0 or col>row-1):
+                tempcol.append(1)
+            else:
+                tempcol.append(ans[row-1][col-1]+ans[row-1][col])
+        ans.append(tempcol)
+
+    return ans
